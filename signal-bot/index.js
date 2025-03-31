9a0372ae0c0415d44121a2ed99f9f2bbe3003e62
olgierd <kogutdmytro@gmail.com>
2025-03-31 13:13:56 +0300

    Send and receive messages via js

--- signal-bot/index.js
+++ signal-bot/index.js
@@ -3,15 +3,15 @@ const http = require('http');
 const SIGNAL_API_URL = 'signal-api';
 const SIGNAL_API_PORT = 8080;
 const YOUR_PHONE_NUMBER = '+380633499278';
-const RECIPIENT_NUMBER = '+380688433139';
 
 console.log(`SIGNAL_API_URL: ${SIGNAL_API_URL}`);
 
-function sendSignalMessage() {
+function sendSignalMessage(recipient, message) {
+    console.log(recipient);
     const postData = JSON.stringify({
-        message: 'Hello from Signal Bot via HTTP!',
+        message: message,
         number: YOUR_PHONE_NUMBER,
-        recipients: [RECIPIENT_NUMBER],
+        recipients: [recipient],
     });
 
     const options = {
@@ -33,7 +33,7 @@ function sendSignalMessage() {
         });
 
         res.on('end', () => {
-            console.log('Message sent successfully:', data);
+            console.log('Message sent successfully. Status code:', res.statusCode, 'Data:', data);
         });
     });
 
@@ -45,10 +45,55 @@ function sendSignalMessage() {
     req.end();
 }
 
-// Виконувати команду кожні 30 секунд
-setInterval(sendSignalMessage, 30000);
+function receiveSignalMessages() {
+    const options = {
+        hostname: SIGNAL_API_URL,
+        port: SIGNAL_API_PORT,
+        path: `/v1/receive/${YOUR_PHONE_NUMBER}`,
+        method: 'GET',
+        headers: {
+            'Accept': 'application/json',
+        },
+    };
 
-// Нескінченний цикл
+    const req = http.request(options, (res) => {
+        let data = '';
+
+        res.on('data', (chunk) => {
+            data += chunk;
+        });
+
+        res.on('end', () => {
+            console.log('Received data: ', data);
+            try {
+                const messages = JSON.parse(data);
+                if (messages && messages.length > 0) {
+                    messages.forEach((messageObject) => {
+                        if (messageObject.envelope.dataMessage
+                            && messageObject.envelope.dataMessage.message
+                            && messageObject.envelope.dataMessage.message.toLowerCase().includes('hello')) {
+                            sendSignalMessage(
+                                messageObject.envelope.sourceNumber,
+                                'Hi! I`m glad to hear you, ' + messageObject.envelope.sourceName
+                            );
+                        }
+                    });
+                }
+            } catch (error) {
+                console.error('Error parsing received messages:', error.message);
+            }
+        });
+    });
+
+    req.on('error', (error) => {
+        console.error('Error receiving messages:', error.message);
+    });
+
+    req.end();
+}
+
+setInterval(receiveSignalMessages, 10000);
+
 setInterval(() => {
     console.log('Bot is running...');
 }, 60000);
